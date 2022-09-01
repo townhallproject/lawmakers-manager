84614a95652ce39ef06279bf0ba0fcc685b422d1
meganrm <meganrm@gmail.com>
2022-08-31 21:32:17 -0700

    update whip script

--- bin/senate-whip.js
+++ bin/senate-whip.js
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 
-require('dotenv').config();
+require("dotenv").config();
 const {
     google
-} = require('googleapis');
+} = require("googleapis");
 
-const readline = require('readline');
+const readline = require("readline");
 
+const firebasedb = require("../lib/setupFirebase.js");
+const googleMethods = require("../lib/google-methods");
 
-const firebasedb = require('../lib/setupFirebase.js');
-const googleMethods = require('../lib/google-methods');
+const SHEETS_ID = "1Dpg0iY1LKTEEzQ80ESZ-nbSXkUkRi8Kjobysu4-AAkA";
 
-const SHEETS_ID = '1Dpg0iY1LKTEEzQ80ESZ-nbSXkUkRi8Kjobysu4-AAkA';
-
-
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 const clientId = process.env.GOOGLE_CLIENT_ID;
@@ -22,11 +20,15 @@ const redirectUrl = process.env.GOOGLE_REDIRECT_URI_1;
 const currentToken = {
     access_token: process.env.GOOGLE_ACCESS_TOKEN,
     refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-    token_type: 'Bearer',
+    token_type: "Bearer",
     expiry_date: 1522106489761,
 };
 
-const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
+const oAuth2Client = new google.auth.OAuth2(
+    clientId,
+    clientSecret,
+    redirectUrl
+);
 oAuth2Client.credentials = currentToken;
 
 class Senator {
@@ -34,12 +36,12 @@ class Senator {
         this.displayName = opts.displayName;
         this.party = opts.party;
 
-        this.issues = issues
+        this.issues = issues;
         this.last_name = opts.last_name || null;
         this.first_name = opts.first_name || null;
         this.govtrack_id = opts.govtrack_id;
         if (!opts.roles) {
-            console.log('no role', opts.displayName);
+            console.log("no role", opts.displayName);
         }
         const office = opts.roles[0];
         this.state = office.state;
@@ -47,22 +49,21 @@ class Senator {
         this.contact = {
             phone: office.phone,
             address: office.address,
-        }
+        };
         this.socials = {
             facebook: opts.facebook,
             twitter: opts.twitter,
-            url: opts.url
-        }
+            url: opts.url,
+        };
     }
     toString() {
-        return this.name + ', ' + this.state + ', ' + this.country;
+        return this.name + ", " + this.state + ", " + this.country;
     }
 }
 
 // Firestore data converter
 var senateConverter = {
     toFirestore: function (senator) {
-
         return {
             displayName: senator.displayName,
             last_name: senator.last_name,
@@ -79,32 +80,53 @@ var senateConverter = {
             socials: {
                 facebook: senator.socials.facebook || null,
                 twitter: senator.socials.twitter || null,
-                url: senator.socials.url || null
-            }
-        }
+                url: senator.socials.url || null,
+            },
+        };
     },
     fromFirestore: function (snapshot, options) {
         const data = snapshot.data(options);
-        return new Senator(data, data.issues)
-    }
-}
-
+        return new Senator(data, data.issues);
+    },
+};
 
 const sheets = google.sheets({
-    version: 'v4',
-    auth: oAuth2Client
+    version: "v4",
+    auth: oAuth2Client,
 });
 
 const getMetaData = (callback) => {
-    return googleMethods.read(sheets, SHEETS_ID, "Whip Count Metadata!A2:M")
-        .then(googleRows => {
+    return googleMethods
+        .read(sheets, SHEETS_ID, "Whip Count Metadata!A2:T")
+        .then((googleRows) => {
             const rows = googleRows.filter((row) => row[0]); //has ID
             const total = rows.length;
             let done = 0;
             const topics = [];
-            let endColumn = "A"
+            let endColumn = "A";
             return rows.forEach(async (row, index) => {
-                const [id, name, description, aboutLink, aboutLinkText, statusText1, statusText2, statusText3, statusText4, statusText5, statusColumn, citationColumn, active] = row
+                const [
+                    id,
+                    name,
+                    description,
+                    aboutLink,
+                    aboutLinkText,
+                    statusText1,
+                    statusText2,
+                    statusText3,
+                    statusText4,
+                    statusText5,
+                    statusColumn,
+                    citationColumn,
+                    active,
+                    abrStatus1,
+                    abrStatus2,
+                    abrStatus3,
+                    abrStatus4,
+                    abrStatus5,
+                    votesNeeded,
+                    passed
+                ] = row;
 
                 const dataToWrite = {
                     id,
@@ -112,48 +134,61 @@ const getMetaData = (callback) => {
                     description,
                     aboutLink,
                     aboutLinkText,
-                    statusText: [statusText1, statusText2, statusText3, statusText4, statusText5],
+                    statusText: [
+                        statusText1,
+                        statusText2,
+                        statusText3,
+                        statusText4,
+                        statusText5,
+                    ],
+                    abrStatusText: [
+                        abrStatus1,
+                        abrStatus2,
+                        abrStatus3,
+                        abrStatus4,
+                        abrStatus5,
+                    ],
+                    votesNeeded,
                     statusColumn,
                     citationColumn,
-                    active: active === 'TRUE'
+                    active: active === "TRUE",
+                    passed: passed === "TRUE"
+                };
+                if (
+                    googleMethods.convertColumnToNumber(citationColumn) >
+                    googleMethods.convertColumnToNumber(endColumn)
+                ) {
+                    endColumn = citationColumn;
                 }
-                if (googleMethods.convertColumnToNumber(citationColumn) > googleMethods.convertColumnToNumber(endColumn)) {
-                    endColumn = citationColumn
-                }
-                topics.push(dataToWrite)
-                return firebasedb.firestore.collection('whip_count_metadata').doc(id).set(dataToWrite)
+                topics.push(dataToWrite);
+                return firebasedb.firestore
+                    .collection("whip_count_metadata")
+                    .doc(id)
+                    .set(dataToWrite)
                     .then(() => {
                         done++;
                         if (done === total) {
                             callback(endColumn, topics);
-
                         } else {
                             return false;
                         }
-                    }).catch((err) => {
-
-                        console.log(dataToWrite, err)
                     })
-
-            })
-
-        })
-}
+                    .catch((err) => {
+                        console.log(dataToWrite, err);
+                    });
+            });
+        });
+};
 
 const getMemberData = (endColumn, topics) => {
-    console.log("LAST COLUMN TO READ", endColumn)
+    console.log("LAST COLUMN TO READ", endColumn);
     googleMethods
         .read(sheets, SHEETS_ID, `[PUBLIC DATA] Whip Count!A2:${endColumn}101`)
-        .then(googleRows => {
+        .then((googleRows) => {
             const total = googleRows.length;
             let done = 0;
             googleRows.forEach(async (row, index) => {
-                const [
-                    memberId,
-                    firstName,
-                    lastName,
-                    party,
-                ] = row;
+                const [memberId, firstName, lastName, party] = row;
                 const formatNumber = (status, defaultValue) => {
                     // if (status) {
                     //     const splitResult = status.split(/(. |.\t)/g)[0];
@@ -162,38 +197,38 @@ const getMemberData = (endColumn, topics) => {
                     //     }
                     // }
                     return status ? status.split(/(. |.\t)/g)[0] : defaultValue;
-                }
+                };
                 const issues = topics.reduce((acc, topic) => {
-                    const statusIndex = googleMethods.convertColumnToNumber(topic.statusColumn) - 1;
-                    const citationIndex = googleMethods.convertColumnToNumber(topic.citationColumn) - 1;
+                    const statusIndex =
+                        googleMethods.convertColumnToNumber(topic.statusColumn) - 1;
+                    const citationIndex =
+                        googleMethods.convertColumnToNumber(topic.citationColumn) - 1;
 
                     const status = formatNumber(row[statusIndex], "3");
                     const citation = row[citationIndex] || "";
                     acc[topic.id] = {
                         status,
                         citation,
-                    }
+                    };
                     return acc;
                 }, {});
 
                 if (!memberId) {
-                    console.log(row)
+                    console.log(row);
                     done++;
                     return;
-
                 }
-                const snapshot = await firebasedb.firestore.collection('office_people').doc(memberId)
+                const snapshot = await firebasedb.firestore
+                    .collection("office_people")
+                    .doc(memberId)
                     .get();
                 const data = snapshot.data();
 
                 if (!data) {
                     done++;
-                    return console.log(memberId)
+                    return console.log(memberId);
                 }
-                const senator = new Senator(
-                    data,
-                    issues,
-                )
+                const senator = new Senator(data, issues);
                 const dataToWrite = senateConverter.toFirestore(senator);
                 // for (const key in dataToWrite) {
                 //     if (Object.hasOwnProperty.call(dataToWrite, key)) {
@@ -202,25 +237,26 @@ const getMemberData = (endColumn, topics) => {
                 //             console.log(dataToWrite.displayName, key)
                 //         }
                 //     }
-                // }   
-                return firebasedb.firestore.collection('whip_count_2020').doc(memberId).set(dataToWrite)
+                // }
+                return firebasedb.firestore
+                    .collection("whip_count_2020")
+                    .doc(memberId)
+                    .set(dataToWrite)
                     .then(() => {
                         done++;
                         if (done === total) {
-                            process.exit(0)
-
+                            process.exit(0);
                         }
-                    }).catch((err) => {
-
-                        console.log(dataToWrite, err)
                     })
-
-            })
+                    .catch((err) => {
+                        console.log(dataToWrite, err);
+                    });
+            });
         })
-        .catch(err => {
-            console.log("error reading sheet:", err.message)
-            process.exit(1)
-        })
-}
+        .catch((err) => {
+            console.log("error reading sheet:", err.message);
+            process.exit(1);
+        });
+};
 
-getMetaData(getMemberData)
+getMetaData(getMemberData);
